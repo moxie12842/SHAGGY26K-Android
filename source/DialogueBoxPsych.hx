da5cc3a0cc97164bad89c2ddc3c36ee29e0528e6
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-04-25 19:52:00 +0800

    Update DialogueBoxPsych.hx

--- source/DialogueBoxPsych.hx
+++ source/DialogueBoxPsych.hx
@@ -148,7 +148,20 @@ class DialogueBoxPsych extends FlxSpriteGroup
 			bgFade.alpha += 0.5 * elapsed;
 			if(bgFade.alpha > 0.5) bgFade.alpha = 0.5;
 
-			if(FlxG.keys.justPressed.ANY) {
+			#if android
+		var justTouched:Bool = false;
+
+		for (touch in FlxG.touches.list)
+		{
+			justTouched = false;
+
+			if (touch.justReleased){
+				justTouched = true;
+			}
+		}
+		#end
+
+		if (FlxG.keys.justPressed.ANY #if android || justTouched #end) {
 				if(!daText.finishedText) {
 					if(daText != null) {
 						daText.killTheTimer();
